09b74eea67d4c30a40b804a9c5436effc2128fec
leopham <hongthaipro@gmail.com>
2024-06-27 11:27:11 +0700

    refactor: Update Navbar component to highlight active menu item

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,16 @@
+"use client";
 import { MENU_LIST } from "@/lib/contants";
 import Image from "next/image";
 import React from "react";
 import { Separator } from "./ui/separator";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 type Props = {};
 
 const Navbar: React.FC<Props> = ({}) => {
+  const pathname = usePathname();
   return (
     <div className="h-screen p-5 shadow-sm border space-y-4">
       <div className="flex items-center gap-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-2 rounded-md shadow-md">
@@ -18,8 +22,14 @@ const Navbar: React.FC<Props> = ({}) => {
         <Separator className="my-6" />
         {MENU_LIST.map((menu) => (
           <Link key={menu.name} href={menu.path}>
-            <div className="flex gap-2 mb-2 p-3  hover:bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:text-white transition-colors duration-300 rounded-md cursor-pointer">
-              <menu.icon />
+            <div
+              className={cn(
+                "flex items-center gap-2 mb-2 p-3  hover:bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:text-white transition-colors duration-300 rounded-md cursor-pointer",
+                pathname === menu.path &&
+                  "bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white"
+              )}
+            >
+              <menu.icon className="h-6 w-6" />
               <h1>{menu.name}</h1>
             </div>
           </Link>
