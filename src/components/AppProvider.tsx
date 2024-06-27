659f2a202348710e393f4317b38fbbcdfca0b2f2
leopham <hongthaipro@gmail.com>
2024-06-27 09:55:13 +0700

    add clrek auth

---
+++ src/components/AppProvider.tsx
@@ -0,0 +1,16 @@
+import React, { PropsWithChildren } from "react";
+
+type Props = {};
+
+const AppProvider: React.FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <main>
+      {children}
+      <div className="h-10 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 fixed bottom-0 w-full flex items-center justify-center">
+        <span className="text-white text-sm">SVNet @ 2024</span>
+      </div>
+    </main>
+  );
+};
+
+export default AppProvider;
